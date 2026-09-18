c2797268be6a14aa82a8d1a91aa68b4db415d4b3
siri <sirinagrajreddy399@gmail.com>
2026-09-18 12:40:29 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,36 @@
 // }
 
 // export default App;
+// import "./App.css";
+// function App(){
+//   const tarinerName="Nirmal";
+//   const trainingDay=3;
+//   return(
+//     <main className="app">
+//       <section className="welcome-card">
+//       <p className="day-label">React Day {trainingDay}</p>
+//       <h1 className="title">College Course Explorer </h1>
+//       <p>Trainer:{tarinerName}</p>
+//       <p>React Learning Project</p>
+//       <p>Used React,JSX,componets,CSS</p>
+//       </section>   
+//     </main>       
+//   );
+// }
+// export default App;
 import "./App.css";
-function App(){
-  const tarinerName="Nirmal";
-  const trainingDay=3;
-  return(
-    <main className="app">
-      <section className="welcome-card">
-      <p className="day-label">React Day {trainingDay}</p>
-      <h1 className="title">College Course Explorer </h1>
-      <p>Trainer:{tarinerName}</p>
-      <p>React Learning Project</p>
-      <p>Used React,JSX,componets,CSS</p>
-      </section>   
-    </main>       
+import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <main id="home">
+        {/* <h1>campus Connect</h1>
+        <p>Welcome to College Event and Clubs</p> */}
+        <Hero />
+      </main>
+      </div>
   );
 }
 export default App;
